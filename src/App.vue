85d997c54a0d468c7c02e60203b523c7ea22c149
juliabosak <1kn15b.bosak@gmail.com>
2019-10-09 21:14:15 +0300

    has added first section

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <cover />
+  </div>
+</template>
+<style lang="scss" src="./scss/style.scss">
+
+</style>
+<script>
+import cover from './components/cover.vue'
+
+export default {
+  name: 'app',
+  components: {
+    cover
+  }
+}
+</script>
+
---
+++ src/components/cover.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="cover">
+		<div class="cover__ellipse bounceInLeft">
+			<div class="cover__ellipse-content">
+				<div class="cover__ellipse-subtitle">
+					<p>
+						Добро пожаловать в игру
+					</p>
+				</div>
+				<div class="cover__ellipse-title">
+					<h1>Помоги <span class="cover__big-title">Фармацевту</span></h1>
+				</div>
+				<div class="cover__ellipse-button">
+					<button class="cover__button">Начать</button>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+<script>
+	export default {
+		name: 'cover',
+	}
+</script>
